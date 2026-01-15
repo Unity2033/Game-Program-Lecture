0376cda34bfd6a7c937afda6ecf8d3bdfc78af85
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-01-15 21:44:14 +0900

    (2026.1.15) 이중 버퍼링

--- Program/source.c
+++ Program/source.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define SIZE 100000
+
 int main()
 {
 #pragma region 파일 입출력
@@ -9,13 +11,43 @@ int main()
 	// 첫 번째 매개 변수 (파일의 이름)
 	// 두 번째 매개 변수 (파일의 입출력 모드)
 
-	FILE * file = fopen("data.txt","w");
+	// FILE * file = fopen("data.txt","w");
+	// 
+	// fputs("Character Information\n", file);
+	// 
+	// fputs("Health : \n", file);
+	// fputs("Attack : \n", file);
+	// fputs("Defense : \n", file);
+	// 
+	// fclose(file);
 
-	fputs("Character Information\n", file);
+#pragma endregion
 
-	fputs("Health : \n", file);
-	fputs("Attack : \n", file);
-	fputs("Defense : \n", file);
+#pragma region 파일 읽기
+
+	FILE* file = fopen("data.txt", "r");
+
+	int count = 0;
+
+	char character = NULL;
+
+	while ((character = fgetc(file)) != EOF)
+	{
+		count++;
+	}
+
+	printf("count : %d\n", count);
+
+	char buffer[SIZE] = { 0, };
+
+	// 첫 번째 매개변수 : 읽은 데이터를 저장할 메모리 버퍼의 포인터 변수
+	// 두 번째 매개변수 : 각 데이터 항목의 크기 
+	// 세 번째 매개변수 : 데이터를 읽어올 항목의 수
+	// 네 번째 매개변수 : 데이터를 읽어올 파일의 포인터 변수
+
+	fread(buffer, sizeof(char), sizeof(buffer), file);
+
+	printf("%s", buffer);
 
 	fclose(file);
 
