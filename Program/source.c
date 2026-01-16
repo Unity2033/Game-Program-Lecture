a2db72d55198ebd18fef17ab3ed10ff58f24e4bd
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-01-16 21:44:03 +0900

    (2026.1.16) 색상 시스템

--- Program/source.c
+++ Program/source.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define SIZE 100000
+void load(const char * fileName)
+{
+	FILE * file = fopen(fileName, "r");
+
+	int count = 0;
+
+	int character = '\0';
+
+	while ((character = fgetc(file)) != EOF)
+	{
+		count++;
+	}
+
+	rewind(file);
+
+	char* buffer = malloc(count + 1);
+
+	buffer[count] = NULL;
+
+	fread(buffer, sizeof(char), count, file);
+
+	// 첫 번째 매개변수 : 읽은 데이터를 저장할 메모리 버퍼의 포인터 변수
+	// 두 번째 매개변수 : 각 데이터 항목의 크기 
+	// 세 번째 매개변수 : 데이터를 읽어올 항목의 수
+	// 네 번째 매개변수 : 데이터를 읽어올 파일의 포인터 변수
+
+	printf("%s", buffer);
+
+	fclose(file);
+
+	free(buffer);
+}
 
 int main()
 {
@@ -25,34 +57,9 @@ int main()
 
 #pragma region 파일 읽기
 
-	FILE* file = fopen("data.txt", "r");
-
-	int count = 0;
-
-	char character = NULL;
-
-	while ((character = fgetc(file)) != EOF)
-	{
-		count++;
-	}
-
-	printf("count : %d\n", count);
-
-	char buffer[SIZE] = { 0, };
-
-	// 첫 번째 매개변수 : 읽은 데이터를 저장할 메모리 버퍼의 포인터 변수
-	// 두 번째 매개변수 : 각 데이터 항목의 크기 
-	// 세 번째 매개변수 : 데이터를 읽어올 항목의 수
-	// 네 번째 매개변수 : 데이터를 읽어올 파일의 포인터 변수
-
-	fread(buffer, sizeof(char), sizeof(buffer), file);
-
-	printf("%s", buffer);
-
-	fclose(file);
+	// load("Resources/Snubbull.txt");
 
 #pragma endregion
-
 
 #pragma endregion
 
